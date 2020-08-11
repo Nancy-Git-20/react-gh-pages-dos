d9399541e2cc7becd85da57cc088a8723b5d993e
Nancy-Git-20 <nancy.pompei@gmail.com>
2020-08-11 01:19:16 -0300

    Updates

---
+++ precache-manifest.d677169d48f11098a13b424154178f43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a778e54c03ddb19d41ff29607eeb363",
+    "url": "/react-gh-pages-dos/index.html"
+  },
+  {
+    "revision": "a7929824c4dd0673c85b",
+    "url": "/react-gh-pages-dos/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "16fa7b2d332f4cf92d63",
+    "url": "/react-gh-pages-dos/static/js/2.8c6ffe29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages-dos/static/js/2.8c6ffe29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7929824c4dd0673c85b",
+    "url": "/react-gh-pages-dos/static/js/main.c9250cf1.chunk.js"
+  },
+  {
+    "revision": "29787d97fdb53b6d89a0",
+    "url": "/react-gh-pages-dos/static/js/runtime-main.ccb1b202.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages-dos/static/media/logo.5d5d9eef.svg"
+  }
+]);
